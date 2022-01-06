689c590400fba85ae0426aabcf1aa1d2c434af9a
Suhas0508 <96803394+Suhas0508@users.noreply.github.com>
2022-01-06 06:04:14 +0530

    Create fibonacci.py
    Fibonacci Series

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+# Fibonacci Series
+# The Fibonacci sequence is a sequence where the next term is the sum of the previous two terms. 
+# The first two terms of the Fibonacci sequence are 0 followed by 1.
+
+
+num = int(input("Enter Number : "))
+f = 0
+s = 1
+rem = 0
+total = f+s
+print(f"{f}, {s}, ",end="")
+for i in range(1,num):
+    
+    print(f"{total}, ", end="")
+    f = s
+    s = total
+    total = f+s
+  
